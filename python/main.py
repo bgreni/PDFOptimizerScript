df9e168393399c70ca169b2ec7c9de84268fc91e
bgreni <grenierb96@gmail.com>
2020-04-26 00:06:07 -0600

    fixed issue when running on windows

--- python/main.py
+++ python/main.py
@@ -1,4 +1,5 @@
 from os import path, system, mkdir
+from sys import platform
 from multiprocessing import Pool, cpu_count
 import time
 import argparse
@@ -14,41 +15,66 @@ class Timer:
         self.end = time.time()
         self.interval = self.end - self.start
 
-# quality options for GhostScript 4 is the highest compression level
-quality = {
-        0: '/default',
-        1: '/prepress',
-        2: '/printer',
-        3: '/ebook',
-        4: '/screen'
-    }
 
-args = None
+class PDFOptimizer:
+    def __init__(self, args):
+        self.args = args
+        # quality options for GhostScript 4 is the highest compression level
+        self.qualityOptions = {
+            0: '/default',
+            1: '/prepress',
+            2: '/printer',
+            3: '/ebook',
+            4: '/screen'
+        }
 
-def optimize(filename: str):
+    def optimize(self, filename: str):
 
-    if args.rename:
-        # add "_Optimized to end of the filename"
-        split = filename.split('/')[-1].split('.')
-        split[0] += '_Optimized'
-        newName = '.'.join(split)
-        outFile = args.outFolder + '/' + newName
-    else:
-        outFile = args.outFolder + '/' + filename.split('/')[-1]
+        if self.args.rename:
+            # add "_Optimized to end of the filename"
+            split = filename.split('/')[-1].split('.')
+            split[0] += '_Optimized'
+            newName = '.'.join(split)
+            outFile = self.args.outFolder + '/' + newName
+        else:
+            outFile = self.args.outFolder + '/' + filename.split('/')[-1]
 
-    # format ghostscript command string
-    command = (f'gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS={quality[args.compressionLevel]}'
-        f' -dNOPAUSE -dQUIET -dBATCH -sOutputFile=\"{outFile}\" \"{filename}\"')
+        # format ghostscript command string
+        if platform == 'win32':
+            command = (f'gswin64c -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS={self.qualityOptions[self.args.compressionLevel]}'
+            f' -dNOPAUSE -dQUIET -dBATCH -sOutputFile=\"{outFile}\" \"{filename}\"')
+        else:
+            command = (f'gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS={self.qualityOptions[self.args.compressionLevel]}'
+            f' -dNOPAUSE -dQUIET -dBATCH -sOutputFile=\"{outFile}\" \"{filename}\"')
 
-    # run command
-    system(command)
+        # run command
+        system(command)
 
-    # get size stats
-    if args.stats:
-        beforeSize = path.getsize(filename)
-        afterSize = path.getsize(outFile)
-        return filename, beforeSize, afterSize
+        # get size stats
+        if self.args.stats:
+            beforeSize = path.getsize(filename)
+            afterSize = path.getsize(outFile)
+            return filename, beforeSize, afterSize
 
+    def run(self):
+        # kinda gross but whatever
+        filesList = list(filter(lambda x: x.endswith('.pdf'), [str(Path(f).resolve()) for f in Path(self.args.inFolder).iterdir()]))
+
+        # create destination folder if it doesn't already exist
+        if not path.exists(self.args.outFolder):
+            mkdir(self.args.outFolder)
+
+        with Timer() as t:
+            with Pool(cpu_count()) as p:
+                returns = p.map(self.optimize, filesList)
+
+        # print time it took
+        print('Time taken:', str(timedelta(seconds=t.interval)))
+
+        # print out size improvement of each file processed
+        if self.args.stats:
+            for filename, beforeSize, afterSize in returns:
+                print('file: ' + filename.split('/')[-1] + ' is now ' + str(round(1 - (afterSize / beforeSize), 2)) + '%% smaller')
 
 if __name__ == '__main__':
 
@@ -64,29 +90,16 @@ if __name__ == '__main__':
     parser.add_argument('--compression-level', dest='compressionLevel', type=int, default=4,
         help='The compression level to use.')
 
-    args = parser.parse_args()
 
-    if not args.inFolder:
+    arguments = parser.parse_args()
+
+    if not arguments.inFolder:
         raise Exception('You have not specified in inFolder')
 
-    if not args.outFolder:
+    if not arguments.outFolder:
         raise Exception('You have not specified in outFolder')
 
-    # kinda gross but whatever
-    filesList = list(filter(lambda x: x.endswith('.pdf'), [str(Path(f).resolve()) for f in Path(args.inFolder).iterdir()]))
+    optimizer = PDFOptimizer(arguments)
+    optimizer.run()
 
-    # create destination folder if it doesn't already exist
-    if not path.exists(args.outFolder):
-        mkdir(args.outFolder)
-
-    with Timer() as t:
-        with Pool(cpu_count()) as p:
-            returns = p.map(optimize, filesList)
-
-    # print time it took
-    print('Time taken:', str(timedelta(seconds=t.interval)))
-
-    # print out size improvement of each file processed
-    if args.stats:
-        for filename, beforeSize, afterSize in returns:
-            print('file: ' + filename.split('/')[-1] + ' is now ' + str(round(1 - (afterSize / beforeSize), 2)) + '%% smaller')
+    
