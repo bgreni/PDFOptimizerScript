46018e59b0da744b7ce1642a9ac38172b0fdc14d
bgreni <grenierb96@gmail.com>
2020-04-26 13:51:39 -0600

    progress %

--- python/main.py
+++ python/main.py
@@ -1,5 +1,5 @@
 from os import path, system, mkdir
-from sys import platform
+from sys import platform, stdout
 from multiprocessing import Pool, cpu_count
 import time
 import argparse
@@ -27,15 +27,22 @@ class PDFOptimizer:
             3: '/ebook',
             4: '/screen'
         }
+        self.filesTotal = 0
 
     def optimize(self, filename: str):
 
         if self.args.rename:
             # add "_Optimized to end of the filename"
-            split = filename.split('/')[-1].split('.')
+            # print('Filename: ',filename)
+            split = filename.split('/')[-1].split('.pdf')
+            # print ('split:',split)
             split[0] += '_Optimized'
-            newName = '.'.join(split)
+            newName = ''.join(split) + '.pdf'
+            # print ('selfargsoutFolder:',self.args.outFolder)
+            # print ('newName:',newName)
             outFile = self.args.outFolder + '/' + newName
+            # outFile = newName
+            # print ('outFile:',outFile)
         else:
             outFile = self.args.outFolder + '/' + filename.split('/')[-1]
 
@@ -60,13 +67,19 @@ class PDFOptimizer:
         # kinda gross but whatever
         filesList = list(filter(lambda x: x.endswith('.pdf'), [str(Path(f).resolve()) for f in Path(self.args.inFolder).iterdir()]))
 
+        self.filesTotal = len(filesList)
         # create destination folder if it doesn't already exist
         if not path.exists(self.args.outFolder):
             mkdir(self.args.outFolder)
 
+        returns = []
         with Timer() as t:
             with Pool(cpu_count()) as p:
-                returns = p.map(self.optimize, filesList)
+                for i, item in enumerate(p.imap_unordered(self.optimize, filesList), 1):
+                    returns.append(item)
+                    stdout.write('\rdone {0:%}'.format(round(i/self.filesTotal), 2))
+        # new line
+        print()
 
         # print time it took
         print('Time taken:', str(timedelta(seconds=t.interval)))
